9628ce6b8bad61552aa89e920ec07b45479cb2c8
Artem <fenris06@rambler.ru>
2023-04-21 21:17:24 +0200

    fix: check cod style

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapstructMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapstructMapper.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.user.mapper;
 import org.mapstruct.Mapper;
 import ru.practicum.shareit.user.dto.UserDTO;
 import ru.practicum.shareit.user.model.User;
+
 @Mapper
 public interface UserMapstructMapper {
 
