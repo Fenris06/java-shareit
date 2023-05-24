17ac83989669154dafa96901989106422ad96dc7
Artem <fenris06@rambler.ru>
2023-05-25 00:07:01 +0200

    feat: check UserServiceIntegrationTest cod style

--- src/test/java/ru/practicum/shareit/user/service/UserServiceIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceIntegrationTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.transaction.annotation.Transactional;
+
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.dto.UserDTO;
 
